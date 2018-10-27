c160feb42e4f9712065ed0241c0e6f32c77b8d91
Angelogeb <any__955@hotmail.it>
2018-10-27 11:26:06 +0200

    Implemented querying

--- hw2/p1/index.py
+++ hw2/p1/index.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 import re
 import pickle
+import heapq
+import linecache
 
-from collections import Counter
+from collections import Counter, namedtuple
 from math import log10, sqrt
+
+PostingListEntry = namedtuple('PostingListEntry', ['docId', 'partialScore'])
+HeapEntry = namedtuple('HeapEntry', ['score', 'docId'])
 
 def remove_special_chars(s):
     return re.sub(re.compile(r'\W|\b[a-zA-Z]{,2}\b'), ' ', s).lower()
 
-
-def preprocess_docs(src_file, dst_file, clean_attrs = {'title', 'description', 'city'}):
+def preprocess_docs(src_file,
+                    dst_file,
+                    clean_attrs = {'title', 'description', 'city'}):
     fin = open(src_file)
     fout = open(dst_file, 'w')
 
@@ -70,11 +76,11 @@ def build_index(src_file, dst_file, readable = False):
 
     (n_docs, df) = doc_frequency(src_file)
 
-    for (docId, doc) in enumerate(fin):
+    for (docId, doc) in enumerate(fin, start = 1):
         attr_values = doc.strip().split('\t')
         tokens = attr_values[attr_i['description']].split()
-        tokens += (attr_values[attr_i['title']].split() * 5) # weights
-        tokens += (attr_values[attr_i['city']].split() * 4)  #
+        tokens += (attr_values[attr_i['title']].split()) # weights
+        tokens += (attr_values[attr_i['city']].split())  #
 
         tf = Counter(tokens)
         num = {} # given a term: tf * idf^2
@@ -85,9 +91,9 @@ def build_index(src_file, dst_file, readable = False):
             num[term] *= log10(n_docs/df[term])
 
         for term in tf:
-            score = num[term] / doc_2norm 
+            partialScore = num[term] / sqrt(doc_2norm) 
             posting_list = index.get(term, [])
-            posting_list.append((docId, score))
+            posting_list.append(PostingListEntry(docId, partialScore))
             index[term] = posting_list
 
     if readable:
@@ -98,15 +104,53 @@ def build_index(src_file, dst_file, readable = False):
 
     fin.close()
     fout.close()
-
+    return index
 
 def load_index(name):
-    res = {}
     with open(name + '.pickle', 'rb') as f:
-        res = pickle.load()
-    return res
+        return pickle.load(f)
+
+def min_heap(heap):
+    return heap[0]
+
+def process_query(q, index, k):
+    q_tf = Counter(remove_special_chars(q).split())
+    heap = [HeapEntry(0,0)]
+    pointers = {t: 0 for t in q_tf if t in index }
+
+    current_docIds = [index[t][pointers[t]].docId for t in pointers]
+    min_docId = min(current_docIds) if current_docIds else -1
+
+    while min_docId != -1:
+        score = 0
+
+        for t in pointers:
+            if pointers[t] < len(index[t]):
+                elem = index[t][pointers[t]]
+                if elem.docId == min_docId:
+                    score += elem.partialScore * q_tf[t]
+                    pointers[t] += 1
+
+        if len(heap) < k:
+            heapq.heappush(heap, HeapEntry(score, min_docId))
+        elif score > min_heap(heap).score:
+            heapq.heappushpop(heap, HeapEntry(score, min_docId))
+
+        current_docIds = [index[t][pointers[t]].docId for t in pointers if pointers[t] < len(index[t])]
+        min_docId = min(current_docIds) if current_docIds else -1
+
+    return sorted(heap, reverse = True)
+
+        
 
 
 if __name__ == '__main__':
+    doc = """Linux System Administrator:  Requisiti ricercati: - Esperienza nel ruolo di Sistemista di almeno 2-3 anni; - Competenze tecniche Sistemi operativi Linux; - Conoscenza di base di integrazione di sistemi informatici; - Conoscenza di linguaggi di scripting, in particolare shell.  Costituiscono titolo preferenziale: - Conoscenza dello stack ELK e/o di altre soluzioni di log management; - Conoscenza di soluzioni di network e data security.  Attività proposta:  La risorsa verrà inserita in un team che gestisce le seguenti attività: - Amministrazione di sistemi Linux; - Troubleshooting sui sistemi Linux; - System Maintenance su hardware e software; - Analisi e patching dei sistemi Linux; - Tuning per le performance e assicurare l'alta affidabilità dell'infrastruttura; - Implementazione ed integrazione soluzioni di log management.  L'opportunità prevede un inserimento con contratto a tempo indeterminato
+    """
     preprocess_docs('file.tsv', 'clean_file.tsv')
-    build_index('clean_file.tsv', 'inverted_index', True)
+    index = build_index('clean_file.tsv', 'inverted_index', True)
+    res = process_query(doc, index, 3)
+    for (score, docId) in res:
+        print("# " + str(docId))
+        print("score: " + str(score))
+        print(linecache.getline("file.tsv", docId + 1))
